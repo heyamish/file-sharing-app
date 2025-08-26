4a2bfdd123eb6b4d0c401b8953bd3756537f3168
Manish <rajmanishdav@gmail.com>
2025-08-26 23:34:58 +0530

    Updated db.js and added .gitignore

--- server/databse/db.js
+++ server/databse/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const DBConnection = async () => {
-    const MONODB_URI = `mongodb+srv://amish:amish@cluster0.ovnvdas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    const MONODB_URI = `mongodb+srv://username:username@your_app_name.ovnvdas.mongodb.net/?retryWrites=true&w=majority&appName=your_app_name`;
     try {
        await mongoose.connect(MONODB_URI, { useNewUrlParser: true });
         console.log('Databse connected successfully');
